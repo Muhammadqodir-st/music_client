a0df1b247179d582cec8d3d7648a736a76441009
Muhammadqodir-st <muhammadqodirov3609@gmail.com>
2026-02-21 06:55:01 +0500

    some fix

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,7 +1,26 @@
+// next
+import Image from "next/image";
+
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="">
+        <div className="min-h-screen flex items-center justify-center">
+            <div className="w-120 h-140  rounded-xl p-5">
+                <h1 className="text-4xl font-bold text-center">Welcome to Music</h1>
 
+                <button className="w-full py-2 rounded-lg flex items-center justify-center gap-5 border">
+                    <Image className="w-7 h-7" src="/assets/google.webp" alt="Google icon" width={50} height={50} />
+                </button>
+
+                <div className="w-full flex items-center gap-3">
+                    <div className="flex-1 h-px bg-white"></div>
+                    <p className="font-bold">OR</p>
+                    <div className="flex-1 h-px bg-white"></div>
+                </div>
+
+                <div>
+                    {children}
+                </div>
+            </div>
         </div>
     )
 }
--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -1,7 +1,16 @@
 export default function Page() {
     return (
-        <div>
-            sign-up
+        <div className="w-full">
+            <form className="w-full" action="">
+                <label htmlFor="nameInput">
+                    <input className="w-full py-2.5 px-3 rounded-lg border" type="text" name="name" id="nameInput" placeholder="Oliver Hayes" />
+                </label>
+                <label htmlFor="emailInput">
+                    <input className="w-full py-2.5 px-3 rounded-lg border" type="text" name="email" id="emailInput" placeholder="name@example.com" />
+                </label>
+
+                <button type="submit" className="w-full p-2.5 rounded-lg border cursor-pointer bg-[#1E90FF]">Sign Up</button>
+            </form>
         </div>
     )
 }
