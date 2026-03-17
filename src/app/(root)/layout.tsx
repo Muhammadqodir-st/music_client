87e926b2fdb2248efac406e10519277e3d2d9fe6
Muhammadqodir-st <muhammadqodirov3609@gmail.com>
2026-03-17 07:43:55 +0500

    some fix

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,14 +1,7 @@
-// components
-import Header from "./components/Header";
-import Sidebar from "./components/Sidebar";
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <main className="min-h-screen flex flex-col">
-            <Header />
-            <div>
-                <Sidebar />
-            </div>
+        <main>
+
         </main>
     );
 };
