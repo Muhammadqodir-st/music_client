df33924a2d349d5624fe5d961646a963e8c4ff12
Muhammadqodir-st <muhammadqodirov3609@gmail.com>
2026-04-29 20:40:29 +0500

    fix

--- src/app/(root)/components/Header.tsx
+++ src/app/(root)/components/Header.tsx
@@ -1,15 +1,28 @@
+"use client"
+
+import { RootState } from "@/store/store";
 import { Search } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
+import { useSelector } from "react-redux";
+
 
 export default function Header() {
-    return (
-        <div className="w-full px-4 py-6 flex">
-            <Image src="/assets/101.png" alt="logo" width={100} height={100} />
+    // user
+    const user = useSelector((state: RootState) => state.user.data)
 
-            <label className="flex border rounded-full px-3" htmlFor="">
-                <input className="outline-0" type="text" />
-                <Search />
+    return (
+        <header className="w-full px-5 py-6 flex items-center justify-between">
+            <Link href={'/'}>
+                <Image src="/assets/101.png" alt="logo" width={100} height={100} />
+            </Link>
+
+            <label className="flex rounded-full px-3 py-1.5 border border-gray-500" htmlFor="">
+                <input className="outline-0 px-2" type="text" />
+                <Search className="text-gray-400 cursor-pointer" />
             </label>
-        </div>
+
+            <span />
+        </header>
     );
 };
